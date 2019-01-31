18ae8d2bcf600ecddd909da0057162087b937978
Chris Anderson <chris.scott.anders@gmail.com>
2019-01-31 16:17:59 -0500

    Add login view

--- src/client/components/Navbar.vue
+++ src/client/components/Navbar.vue
@@ -15,6 +15,7 @@
 
     <!-- Right aligned nav items -->
     <b-navbar-nav class="ml-auto">
+      <b-nav-item to="/login">Login</b-nav-item>
       <b-nav-item-dropdown right>
         <!-- Using button-content slot -->
         <template slot="button-content">
@@ -30,17 +31,7 @@
 </template>
 
 <style>
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 body {
-    padding-top: 50px;
+    padding-top: 70px;
 }
 </style>
---
+++ src/client/views/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <b-container id="login">
+    <b-row align-h="center">
+      <b-col cols="4">
+        <b-form @submit="onSubmit">
+          <b-form-group id="emailInputGroup"
+                        label="Email address:"
+                        label-for="email">
+            <b-form-input id="email"
+                          type="email" required=""
+                          v-model="form.email"
+                          placeholder="Your Email">
+            </b-form-input>
+          </b-form-group>
+          <b-form-group id="passwordInputGroup"
+                        label="Password:"
+                        label-for="password">
+            <b-form-input id="password"
+                          type="password" required=""
+                          v-model="form.password"
+                          placeholder="Your Password">
+            </b-form-input>
+          </b-form-group>
+          <b-button type="submit" variant="info" block>Log in</b-button>
+        </b-form>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit (event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#login {
+  padding-top: 30px
+}
+</style>
