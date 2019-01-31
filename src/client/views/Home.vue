09c1472f55f86d35cd2538f08fb4d75c68d63877
Chris Anderson <chris.scott.anders@gmail.com>
2019-01-31 16:17:52 -0500

    Remove some boilerplate code

--- src/client/views/Home.vue
+++ src/client/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -16,3 +15,9 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.home {
+  padding: 30px;
+}
+</style>
